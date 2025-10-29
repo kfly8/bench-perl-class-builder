5d2415a96060fee3a54dc186734e71eefac63c72
Bodo (Hugo) Barwich <b.barwich@hotmail.com>
2025-10-29 17:54:39 +0300

    use `writer` attribute on Perl 5.42

--- lib/Foo/FeatureClass.pm
+++ lib/Foo/FeatureClass.pm
@@ -2,12 +2,8 @@ use v5.40;
 use experimental qw(class);
 
 class Foo::FeatureClass {
-    field $foo :param :reader;
-    field $bar :param :reader;
-    field $baz :param :reader;
-
-    method set_foo( $value ) { $foo = $value; }
-    method set_bar( $value ) { $bar = $value; }
-    method set_baz( $value ) { $baz = $value; }
+    field $foo :param :reader :writer;
+    field $bar :param :reader :writer;
+    field $baz :param :reader :writer;
 };
 
--- lib/Foo/ObjectPad.pm
+++ lib/Foo/ObjectPad.pm
@@ -2,8 +2,8 @@ use v5.40;
 use Object::Pad;
 
 class Foo::ObjectPad {
-    field $foo :reader :writer :param;
-    field $bar :reader :writer :param;
-    field $baz :reader :writer :param;
+    field $foo :param :reader :writer;
+    field $bar :param :reader :writer;
+    field $baz :param :reader :writer;
 };
 
